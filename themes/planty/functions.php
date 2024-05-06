f4fc2cf1bdbb52252932c8cee77480374aec0a33
ChaLeVerdier <contact@art-graphic-leverdier.com>
2024-05-06 15:15:22 +0200

    Envoi après débug du theme enfant

--- themes/planty/functions.php
+++ themes/planty/functions.php
@@ -1,9 +1,14 @@
 <?php
 
-// Action qui permet de charger des scripts dans notre thème
-add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function child_theme_enqueue_styles() {        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css', false, '1.0', 'all');        wp_enqueue_style('main-css', get_stylesheet_directory_uri() . '/style.css', array('parent-style'), '1.0', 'all');}
-add_action('wp_enqueue_scripts','child_theme_enqueue_styles');
+/**
+ * Enqueue child styles.
+ */
+function child_theme_enqueue_styles()
+{
+    //chargement du thème parent
+    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css'); 
+    //chargement du thème enfant
+    wp_enqueue_style('child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 1);
+}
 
-
-
+add_action('wp_enqueue_scripts', 'child_theme_enqueue_styles');
